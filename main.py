9f1745994b0e7b69865f9c6aee8c146a7e64a0e3
Ankit Kumar <148523859+Ankitkr1375@users.noreply.github.com>
2025-06-28 00:06:11 +0530

    Update main.py

--- main.py
+++ main.py
@@ -31,10 +31,5 @@ async def predict(
     return JSONResponse(content={"prediction": label})
 
 
-if __name__ == "__main__":
-    import uvicorn
-    import os
 
-    port = int(os.environ.get("PORT", 8000))
-    uvicorn.run("main:app", host="0.0.0.0", port=port)
 
